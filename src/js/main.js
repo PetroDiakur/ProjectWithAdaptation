9230afe33e5a567f7054753ef7dddea1c04c9457
PetroDiakur <petrodiakur@gmail.com>
2023-12-04 21:30:01 +0100

    Add mobile menu & addapted to the phone size

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 import "../scss/style.scss";
+const openMenu = document.querySelector(".main-header__button");
+const menu = document.querySelector(".main-header__list");
+const closeMenu = document.querySelector(".main-header__cross");
+openMenu.addEventListener("click", () => {
+  menu.classList.add("open");
+});
+
+closeMenu.addEventListener("click", () => {
+  menu.classList.remove("open");
+});
